a30eb4d0f1ee6bd3ced541ea33b78958190b505a
Anujkj <anujjaimini045@gmail.com>
2023-01-23 11:26:47 +0530

    correction in q2

--- javascriptAssignment/q2.js
+++ javascriptAssignment/q2.js
@@ -17,9 +17,21 @@ MetadataParser.prototype.getKeyField = function(){
 }
 
 MetadataParser.prototype.getArray = function(data){
-    // loop find data and put in array
+    
+    let key = this._keyField;
+    let result = [];
+    data.map((obj) => {
+        if(obj.key){
+            result.push(obj.key);
+        }
+    });
+    return result;
 }
 
 const data = new MetadataParser(2.0, "A", "key");
 
 console.log(data.getVersion(), data.getChannel(), data.getKeyField());
+
+
+let newData = [{"key":"1"},{"newKey":"2"}, {"key":"3"}, {"key":"4"}];
+console.log(data.getArray(newData));
