bbe33c572268f9567b904910a03571896b2074e6
Anujkj <anujjaimini045@gmail.com>
2023-01-23 11:40:08 +0530

    correction in q5

--- javascriptAssignment/q5.js
+++ javascriptAssignment/q5.js
@@ -3,8 +3,8 @@ const SortArray = function(originalArray){
     this._sortedArray = [].concat(originalArray).sort((a,b) => a-b);
 }
 
-SortArray.prototype.sortArray = function(objectArray){
-
+SortArray.prototype.getOriginalArray = function(){
+    return this._originalArray;
 }
 
 SortArray.prototype.getSortedArray = function(){
@@ -16,3 +16,16 @@ const SortObjectArray = function(originalArray){
 }
 SortObjectArray.prototype = Object.create(SortArray.prototype);
 
+SortObjectArray.prototype.sortArray = function(){
+    return this._sortedArray;
+}
+
+let data = [2,4,7,1,13,5];
+
+const obj = new SortArray(data);
+console.log(obj.getSortedArray(), obj.getOriginalArray());
+
+const newData = [{"1":"1"}, {"3":"3"}, {"2":"2"}];
+
+let newObj = new SortObjectArray(newData);
+console.log(newObj.sortArray());
